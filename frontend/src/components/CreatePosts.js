9707cce1c54bb5f2e3d16533287f59c8a376c785
Kenya Sullivan <kenya@stackpoets.com>
2017-10-05 11:10:58 -0400

    Scaffold new CreatePosts component

---
+++ frontend/src/components/CreatePosts.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class CreatePosts extends Component {
+  render(){
+    return (
+      <div>This is a New Post Form</div>
+    )
+  }
+}
+
+export default CreatePosts;
