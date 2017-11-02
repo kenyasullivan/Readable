7a32dcb2078f1f3ebf930c6a4500193669255262
Kenya Sullivan <kenya@stackpoets.com>
2017-11-02 17:13:00 -0400

    Update CREATEP_POST action

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
-import uuid from 'uuid'
+import axios from "axios";
+import uuid from "uuid";
 
-export const FETCH_POSTS = 'FETCH_POSTS';
-export const CREATE_POSTS = 'CREATE_POSTS'
-export const FETCH_POST = 'FETCH_POST';
-export const DELETE_POST = 'DELETE_POST'
-export const EDIT_POST = 'EDIT_POST'
+export const FETCH_POSTS = "FETCH_POSTS";
+export const CREATE_POSTS = "CREATE_POSTS";
+export const FETCH_POST = "FETCH_POST";
+export const DELETE_POST = "DELETE_POST";
+export const EDIT_POST = "EDIT_POST";
 
-const ROOT_URL='http://localhost:3001'
-const AUTH = {headers:{"Authorization": "Its me!"}}
-axios.defaults.headers.common['Authorization'] = AUTH;
+const ROOT_URL = "http://localhost:3001";
+const AUTH = { headers: { Authorization: "Its me!" } };
+axios.defaults.headers.common["Authorization"] = AUTH;
 // export function fetchPosts() {
 // const request = axios.get(`${ROOT_URL}/posts`, AUTH )
 //     return {
@@ -19,52 +19,73 @@ axios.defaults.headers.common['Authorization'] = AUTH;
 // }
 
 export function fetchPosts() {
- const request = axios.get(`${ROOT_URL}/posts`);
+  const request = axios.get(`${ROOT_URL}/posts`);
   return dispatch => {
-    request.then(({data})=> {
-      dispatch({type: FETCH_POSTS, payload: data})
-  });
-}//end dispatch
-}//end fetchPosts
+    request.then(({ data }) => {
+      dispatch({ type: FETCH_POSTS, payload: data });
+    });
+  }; //end dispatch
+} //end fetchPosts
 
+// export function createPosts(values, callback) {
+
+//   const request = axios.post(`${ROOT_URL}/posts`, values) //request to server
+
+//       return dispatch => {
+//         request.then(({values}) => {
+//           dispatch({type: CREATE_POSTS, payload: request})
+//           callback()  //callback to handle redirect after posts are added to page
+//         })
+//       }
+// }
 export function createPosts(values, callback) {
+  const { title, body, author, category } = values;
 
-  const request = axios.post(`${ROOT_URL}/posts`, values) //request to server
-    
-      return dispatch => {
-        request.then(({values}) => {
-          dispatch({type: CREATE_POSTS, payload: request})
-          callback()  //callback to handle redirect after posts are added to page
-        }) 
-      }
+  const data = {
+    id: uuid.v4(),
+    timestamp: Date.now(),
+    title,
+    author,
+    category,
+    body
+  };
+  const request = axios.post(`${ROOT_URL}/posts`, data); // request to server
+  return dispatch => {
+    request.then(response => {
+      dispatch({
+        type: CREATE_POSTS,
+        payload: response.data
+      });
+      callback();
+    });
+  };
 }
 
-export function fetchPost (id) {
+export function fetchPost(id) {
   const request = axios.get(`${ROOT_URL}/posts/${id}`);
   return dispatch => {
-    request.then(({data})=> {
-      dispatch({type: FETCH_POST, payload: data})
+    request.then(({ data }) => {
+      dispatch({ type: FETCH_POST, payload: data });
     });
-  }
+  };
 }
 
 export function deletePost(id, callback) {
   const request = axios.delete(`${ROOT_URL}/posts/${id}`);
   return dispatch => {
-    request.then(({id})=> {
-      dispatch({type: DELETE_POST, payload: id})
-      callback()//callback to handle redirect after posts are added to page
+    request.then(({ id }) => {
+      dispatch({ type: DELETE_POST, payload: id });
+      callback(); //callback to handle redirect after posts are added to page
     });
-  }
+  };
 }
 
 export function editPost(id, callback) {
   const request = axios.put(`${ROOT_URL}/posts/${id}`);
   return dispatch => {
-    request.then(({data})=> {
-      dispatch({type: EDIT_POST, payload: data})
-      callback()//callback to handle redirect after posts are added to page
+    request.then(({ data }) => {
+      dispatch({ type: EDIT_POST, payload: data });
+      callback(); //callback to handle redirect after posts are added to page
     });
-  }
+  };
 }
-
