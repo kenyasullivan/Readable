1d695901612f6416f95e27d568b5b3a909645281
Kenya Sullivan <kenya@stackpoets.com>
2017-11-03 09:47:40 -0400

    Move Add Post Button to Main Page Only

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import { Container, Header, Button, Divider, Grid } from 'semantic-ui-react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Header, Button, Divider, Grid } from "semantic-ui-react";
 
 const Nav = () => (
   <div>
-<Container text fluid>
-<Header as="h1" textAlign="center">
-Readable
-</Header>
-</Container>
-<Divider/>
-<br/>
-<Container>
-<Grid floated right columns={4}>
-    <Grid.Column>
-   <Link to="/posts/new"> <Button primary>Add Post</Button></Link>
-    </Grid.Column>
-</Grid>
-</Container>
-</div>
+    <Container text fluid>
+      <Header as="h1" textAlign="center">
+        Readable
+      </Header>
+    </Container>
+    <Divider />
+    <br />
+  </div>
 );
 
 export default Nav;
--- frontend/src/components/PostsList.js
+++ frontend/src/components/PostsList.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import _ from "lodash";
 import { Link } from "react-router-dom";
-import { List, Button } from "semantic-ui-react";
+import { List, Button, Container, Grid } from "semantic-ui-react";
 
 //Wire component to action creators
 import { connect } from "react-redux";
@@ -11,7 +11,7 @@ class PostsList extends Component {
   //lifecycle method to initial call to API
   componentDidMount() {
     this.props.fetchPosts();
-    this.props.fetchCategories()
+    this.props.fetchCategories();
   }
 
   //helper function to render posts: map over posts and render 1 <li> for each
@@ -21,62 +21,76 @@ class PostsList extends Component {
     return _.map(this.props.posts, post => {
       return (
         <div className="ui card">
-      <div className="content">
-        <i className="right floated trash outline icon" />
-        <Link to={`/posts/edit/${post.id}`}>
-          <i className="right floated edit icon" />
-        </Link>
-        <div className="header">
-          <Link to={`/posts/${post.id}`}>{post.title}</Link>
+          <div className="content">
+            <i className="right floated trash outline icon" />
+            <Link to={`/posts/edit/${post.id}`}>
+              <i className="right floated edit icon" />
+            </Link>
+            <div className="header">
+              <Link to={`/posts/${post.id}`}>{post.title}</Link>
+            </div>
+          </div>
+          <div className="extra content">
+            <span className="left floated like">
+              <i className="comments outline icon" />
+              {post.commentCount} Comments
+            </span>
+            <span className="right floated star">
+              <i className="chevron up icon" />
+              {post.voteScore}
+              <i className="chevron down icon" />
+            </span>
+          </div>
         </div>
-      </div>
-      <div className="extra content">
-        <span className="left floated like">
-          <i className="comments outline icon" />
-          {post.commentCount} Comments
-        </span>
-        <span className="right floated star">
-          <i className="chevron up icon" />
-          {post.voteScore}
-          <i className="chevron down icon" />
-        </span>
-      </div>
-    </div>
       );
     });
   }
 
   renderCategories() {
-		const {categories} = this.props;
-		if (categories) {
-			return categories.map(category => {
-				return (
-					<li key={category.path} className="list-group-item">
-						<a href={`/${category.name}`}>{category.name}</a>
-					</li>
-				);
-			});
-		}
-	}
+    const { categories } = this.props;
+    if (categories) {
+      return categories.map(category => {
+        return (
+          <li key={category.path} className="list-group-item">
+            <a href={`/${category.name}`}>{category.name}</a>
+          </li>
+        );
+      });
+    }
+  }
   render() {
     //console.log(this.props.posts) // test we are receiving posts from state
-    return (   
-     <div className="ui container">
-    <div className="ui two column grid">
-    <div className="twelve wide column">
-        {/*Render Posts to the Page*/}
-        <h3>Posts</h3>
-        <ul className="ui one cards">{this.renderPosts()}</ul>
+    return (
+      <div>
+        <Container>
+          <Grid floated right columns={4}>
+            <Grid.Column>
+              <Link to="/posts/new">
+                {" "}
+                <Button primary>Add Post</Button>
+              </Link>
+            </Grid.Column>
+          </Grid>
+        </Container>
+        <div className="ui container">
+          <div className="ui two column grid">
+            <div className="twelve wide column">
+              {/*Render Posts to the Page*/}
+              <h3>Posts</h3>
+              <ul className="ui one cards">{this.renderPosts()}</ul>
+            </div>
+            <div className="four wide column">
+              {" "}
+              <h3>Category</h3>
+              <List divided relaxed size={"big"}>
+                <List.Item>
+                  <List.Content>{this.renderCategories()} </List.Content>
+                </List.Item>
+              </List>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="four wide column"> <h3>Category</h3>
-      <List divided relaxed size={"big"}>
-      <List.Item>
-        <List.Content>{this.renderCategories()} </List.Content>
-      </List.Item>
-    </List>
-    </div>
-    </div>
-    </div> 
     );
   }
 }
@@ -90,4 +104,6 @@ function mapStateToProps(state) {
   };
 }
 //get action creator as prop
-export default connect(mapStateToProps, { fetchPosts, fetchCategories })(PostsList);
+export default connect(mapStateToProps, { fetchPosts, fetchCategories })(
+  PostsList
+);
