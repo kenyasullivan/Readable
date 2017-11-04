f301646d8fcb4ec2c9afc63af6d6a98ed58131cd
Kenya Sullivan <kenya@stackpoets.com>
2017-11-04 09:51:51 -0400

    Add Comments Component

--- frontend/src/components/PostDetails.js
+++ frontend/src/components/PostDetails.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { Item, Container, Button } from "semantic-ui-react";
 import { fetchPost, deletePost, editPost } from "../actions";
+import Comments from "./Comments";
 
 class PostDetails extends Component {
   componentDidMount() {
@@ -28,21 +30,34 @@ class PostDetails extends Component {
       return <div>Loading...</div>;
     }
     return (
-      <div className="ui container">
+      <Container>
         <Link to="/">Back To Home</Link>
-        <div className="content">
-        <i className="right floated trash outline icon" />
-        <Link to={`/posts/edit/${post.id}`}>
-          <i className="right floated edit icon" />
-        </Link>
-        <div className="header">
-          <Link to={`/posts/${post.id}`}>{post.title}</Link>
-        </div>
-        <div className="description">
-          <p>{post.body}</p>
-        </div>
-      </div>
-      </div>
+        <Item.Group divided>
+          <Item>
+            <Item.Content>
+              <Item.Header as="a">{post.title}</Item.Header>
+              <Item.Meta>
+                <span className="cinema">
+                  Submited: {post.timestamp} By:{post.author} in {post.category}
+                </span>
+              </Item.Meta>
+              <Container>
+                <Item.Description>{post.body}</Item.Description>
+              </Container>
+              <Item.Extra>
+                <Link to={`/posts/edit/${post.id}`}>
+                  <Button color="red">Edit</Button>
+                </Link>
+                <Button color="blue" onClick={this.onDeleteSubmit.bind(this)}>
+                  Delete
+                </Button>
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+
+        <Comments />
+      </Container>
     );
   }
 }
